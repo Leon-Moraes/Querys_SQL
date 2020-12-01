4f743754cc72cfc1420a1aeac7d8f6dd2fb7b6d0
Leon-Moraes <leo_o_nardo1@outlook.com>
2020-12-01 11:39:17 -0300

    Desenvolvimento do Spring project hello world

---
+++ Spring_projects/HelloWorld/src/main/java/com/example/demo/helloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class helloWorldController {
+	@GetMapping("/app1")
+	public String hello1() {                                           
+		return "index.html";
+	}
+	@GetMapping("/app2")
+	public String hello2() {                                           
+		return "index2.html";
+	}
+}
