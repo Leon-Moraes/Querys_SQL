0602dfceb295d76b04ca5aa4b5924a20d69f6542
Leon-Moraes <leo_o_nardo1@outlook.com>
2020-12-02 14:14:57 -0300

    Desenvolvimento da API - Blog Pessoal

---
+++ Spring_projects/BlogPessoal/src/main/java/com/example/demo/controller/PostagemController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Postagem;
+import com.example.demo.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+	
+	
+//GET METHOD -----------------------------------------------------------
+	
+	@GetMapping("/findAllPostagem")
+	public ResponseEntity<List<Postagem>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/findByIDPostagem")
+	public ResponseEntity<Optional<Postagem>> GetById(@PathVariable Long id) {
+		return ResponseEntity.ok(repository.findById(id));
+	}
+}
