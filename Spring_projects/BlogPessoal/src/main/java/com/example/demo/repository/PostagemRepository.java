10bb2dc9c458004c03d2af860c568f6d7330e47d
Leon-Moraes <leo_o_nardo1@outlook.com>
2020-12-03 10:44:13 -0300

    Adicionando os metodos Put e Delete

--- Spring_projects/BlogPessoal/src/main/java/com/example/demo/repository/PostagemRepository.java
+++ Spring_projects/BlogPessoal/src/main/java/com/example/demo/repository/PostagemRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Postagem;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 }
