5337bb79289720318f8dbce19dce8c68138f78f6
Leon-Moraes <leo_o_nardo1@outlook.com>
2020-12-07 16:13:17 -0300

    Correção de bugs 07/12

--- Spring_projects/BlogPessoal/src/main/java/com/example/demo/controller/TemaController.java
+++ Spring_projects/BlogPessoal/src/main/java/com/example/demo/controller/TemaController.java
@@ -1,11 +1,8 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import java.util.Optional;
-
 import com.example.demo.model.Tema;
 import com.example.demo.repository.TemaRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +11,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,33 +20,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tema")
 public class TemaController {
     @Autowired
-    private TemaRepository repository;
-
+    public TemaRepository repository;
+    
     @GetMapping("/findAllTema")
     public ResponseEntity<List<Tema>> getAll() {
         return ResponseEntity.ok(repository.findAll());
     }
 
     @GetMapping("/findById/{id}")
-    public Optional<Object> getById(@PathVariable long id) {
-        return repository.findById(id).map(resp -> ResponseEntity.ok(resp).orElse(ResponseEntity.notFound().build())));
+    public ResponseEntity<Tema> getById(@PathVariable long id) {
+        return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/findByDescricaoPostagem/{nome}")
     public ResponseEntity<List<Tema>> getByName(@PathVariable String nome) {
-        return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(nome);
+        return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(nome));
     }
 
     @PostMapping("/postTema")
     public ResponseEntity<Tema> post(@RequestBody Tema tema){
-        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
     }
 
-    @PutMapping ("/putTema")
-    ResponseEntity<Tema> put(@RequestBody Tema tema) {
-        return ResponseEntity.ok(repository.save(tema));
-    }
-    
     @DeleteMapping("deleteTema/{id}")
     public void delete(@PathVariable long id ){
         repository.deleteById(id);
--- Spring_projects/BlogPessoal/src/main/java/com/example/demo/repository/TemaRepository.java
+++ Spring_projects/BlogPessoal/src/main/java/com/example/demo/repository/TemaRepository.java
@@ -7,7 +7,7 @@ import com.example.demo.model.Tema;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TemaRepository extends JpaRepository<Tema, Long> {
-    public List<Tema> findAllByDescricaoContainingIgnoreCase( String descricao);
+    public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
 
 }
     
